e658a2445b821eb723837163cf99e8440abbe589
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-02-01 21:46:38 +0900

    [Add]練習問題

---
+++ htdocs/Chapter2/prac.php
@@ -0,0 +1,11 @@
+<!-- 問題1 -->
+<?php
+const DISCOUNT = 0.9;
+$price = 500;
+$sum = $price * DISCOUNT;
+print "値引き後の価格は{$sum}です。";
+
+// 問題2
+var_dump((int)155.36E2);
+$fruit = ['りんご', 'みかん', 'かき'];
+$vegetavles = ['red' => 'トマト', 'yellow' => 'カボチャ','green' => 'レタス'];
