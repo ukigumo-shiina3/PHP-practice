86a64a824dce040bfd470603f43896ffbbfc9a0a
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-02-01 23:28:07 +0900

    [Add]インクリメント演算子

---
+++ htdocs/chapter3/arithmetic.php
@@ -0,0 +1,14 @@
+<?php
+print '108' + '15';     // 123
+print '<br />';
+print '108' + '1.5XYZ'; // 109.5
+print '<br />';
+print '108' + 'XYZ10'; // 108
+print '<br />';
+print '108' + '1.5XYZ10'; // 109.5
+print '<br />';
+print '108' + '0b11'; //108
+print '<br />';
+print '108' + '010'; //118
+print '<br />';
+print '108' + '0x1A'; //108
